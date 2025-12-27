7120753add4ca2dcd3c6698bcfbf32a76e113bbd
petraKn1989 <knichalova@seznam.cz>
2025-12-27 17:04:54 +0100

    create seo component on load and add seznam meta name

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { Seo } from './seo/seo';
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Seo, CommonModule],  // ← NgIf je nutné přidat
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('anketa-app');
+  isHomePage = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        let path = this.router.url;
+
+        if (path.includes('#')) {
+          path = path.split('#')[1]; 
+        }
+
+        this.isHomePage = !path || path === '/';
+      });
+  }
 }
